e87327fcaf9c76ed9389a6b937cf6c3648aba0bf
Igor Pavloski <Igor.Pavloski@seavus.com>
2020-04-26 21:45:47 +0200

    Added fetch in script.js

--- js/script.js
+++ js/script.js
@@ -1,1 +1,14 @@
 console.log("Javscript is connected");
+
+fetch(
+  "https://raw.githubusercontent.com/pavloskii/SEDC-GROUP1-PROJECT-SETUP/master/data/smothies.json"
+)
+  .then(function (response) {
+    return response.json();
+  })
+  .then(function (data) {
+    console.log(data);
+  })
+  .catch(function (error) {
+    console.log(error);
+  });
