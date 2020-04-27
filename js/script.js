6468ab8e11f945bbb201bee401ebb68fb82429ae
Igor Pavloski <Igor.Pavloski@seavus.com>
2020-04-27 15:06:35 +0200

    Dodaden e folderot data so json fajlovi

--- js/script.js
+++ js/script.js
@@ -12,3 +12,33 @@ fetch(
   .catch(function (error) {
     console.log(error);
   });
+
+//За запишување на податоци во firebase
+fetch("https://ime-na-proektot.firebaseio.com/students.json", {
+  method: "POST",
+  body: JSON.stringify({
+    name: "Igor",
+    age: 28,
+  }),
+})
+  .then(function (response) {
+    return response.json();
+  })
+  .then(function (data) {
+    console.log(data);
+  })
+  .catch(function (error) {
+    console.log(error);
+  });
+
+//За преземање на податоци од firebase
+fetch("https://ime-na-proektot.firebaseio.com/students.json")
+  .then(function (response) {
+    return response.json();
+  })
+  .then(function (data) {
+    console.log(data);
+  })
+  .catch(function (error) {
+    console.log(error);
+  });
